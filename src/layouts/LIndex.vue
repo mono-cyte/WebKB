934cdb01c4f526e80e6eba12fd0d19f94bcacb4d
mono-cyte <a578448983@gmail.com>
2025-11-17 15:44:58 +0800

    Update contents

--- src/layouts/LIndex.vue
+++ src/layouts/LIndex.vue
@@ -2,7 +2,55 @@
 
 </script>
 
-
 <template>
-  <div>This is layout for index page</div>
+  <section>
+    <div class="header">
+      <nav>导航栏</nav>
+    </div>
+
+    <div class="body">
+      主体
+
+    </div>
+    <div class="footer">
+      <p>© 2018 Gandalf</p>
+    </div>
+  </section>
+
+
+
 </template>
+
+
+<style scoped lang="less">
+/* 确保html和body有100%高度 */
+
+
+/* 布局容器 */
+#app {
+  height: 100vh;
+}
+
+section {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  .header {
+    min-height: 3rem;
+    background-color: #f0f0f0;
+  }
+
+  .body {
+    flex: 1;
+    background-color: #7b9e37;
+  }
+
+  .footer {
+    min-height: 3rem;
+    background-color: #ff6464;
+  }
+
+}
+</style>
